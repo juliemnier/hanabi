148d5a318941f46b8d1e5eb83483a1b11bdf4c26
Gabriel HADJERCI <hadjerci@athena03.ensta.fr>
2019-04-09 17:58:00 +0200

    Nouveau test

--- test/statistics.py
+++ test/statistics.py
@@ -3,11 +3,26 @@ import hanabi.ai as ai
 
 
 score=[]
+red_coins=[]
+mean=0
+mean_red_coin=0
+lose=0
 for i in range (1000):
     game = hanabi.Game(2)
     AI = ai.RandomPlaying(game)
     game.ai = AI
     game.run()
     score.append(game.score)
+    mean_red_coin+=game.red_coins
+    red_coins.append(game.red_coins)
+    if game.red_coins == 3: lose+=1
+    mean+=game.score
 
-print(score)
+mean=mean/1000
+mean_red_coin/=1000
+#print(score)
+print("Le max est",max(score),".")
+print("Le min est",min(score),".")
+print("Le score moyen est",mean,".")
+print("L'IA à perdu ",lose," fois.")
+print("Ils restaient en moyenne",3-mean_red_coin,"jeton(s) rouge(s).")
