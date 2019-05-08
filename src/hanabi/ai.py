d4a69cf4ca41b0e82a821c0f0b878fdcc7a6bb9b
Gabriel HADJERCI <hadjerci@yoda14.ensta.fr>
2019-05-08 13:21:45 +0200

    Test de notre ai sur ai.py

--- src/hanabi/ai.py
+++ src/hanabi/ai.py
@@ -16,7 +16,7 @@ class AI:
         self.nb_joueurs = len(game.players) #Number of players
         self.list_deduction=[[] for i in range(self.nb_joueurs)] #A list of lists deductions (cf self.deduction) 
         self.actions=[[] for i in range(self.nb_joueurs)] #A list with the memory of the previous actions for each player.[action]
-        self.list_changed=[[] for i in range(self.nb_joueurs)] #A list with the memory of the card which have changed for each player during the turn of another player.[[[cards changed by the action of the player 1],[...player 2]]]
+        self.list_changed=[[] for i in range(self.nb_joueurs)] #A list with the memory of the card which have changed for each player during the turn of another player.[[[index of the card,card changed]]]
     
         
     @property
@@ -251,4 +251,260 @@ class MeilleureAI(AI):
         """
         return the best action possible
         """
+        
         game = self.game
+        #attention : à changer si on change always_playable(self)
+        deduction=self.list_deduction[(self.c_turn)%self.nb_joueurs]
+        changed=self.list_changed[(self.c_turn)%self.nb_joueurs]
+
+         #updating deduction with the card played in the player previous turn
+
+        if self.c_turn<self.nb_joueurs:
+            deduction=self.deduction()
+        else:
+            if self.actions[(self.c_turn)%self.nb_joueurs][0]=='p' or self.actions[(self.c_turn)%self.nb_joueurs][0]=='d':
+                deduction.pop(int(self.actions[(self.c_turn)%self.nb_joueurs][1])-1)
+                deduction.append([[1,2,3,4,5],list(hanabi.deck.Color)])
+
+        self.list_deduction[(self.c_turn)%self.nb_joueurs]=deduction
+        playable=self.always_playable(deduction)
+        #discardable=self.always_discardable(game.current_hand)
+
+
+
+
+        #deductions : for now, the clue is only to be given to the next player.
+
+        prev_action=self.actions[(self.c_turn-1)%self.nb_joueurs]
+        liste_rank=list(game.piles.values())
+
+        if prev_action:
+            if (prev_action[0]=='c'):
+                #if only one card is concerned (cf. strategy)
+                if len(changed)==1:
+                    if changed[0][1].number_clue is False or changed[0][1].color_clue is False:
+                        #play the card without question
+                        self.list_changed[(self.c_turn)%self.nb_joueurs]=[]
+                        self.actions[(self.c_turn)%self.nb_joueurs]= "p%d"%changed[0][0]
+                        self.c_turn=self.c_turn+1
+                        return "p%d"%changed[0][0]
+                
+                # if prev_action[1].isdigit():
+                #   if ((int(prev_action[1])-1) in liste_rank):
+                #on utilise self.deduction() mais jsp comment
+                #      self.deduction()
+                #     for card in changed:
+                #        playable.append(card)
+                # trouve=False
+                # for rank in liste_rank:
+                #    if rank>=prev_action[1]:
+                #       trouve=True
+                #      break
+                # if not trouve:
+                #    for card in changed:
+                #       discardable.append(card)
+
+
+
+        #if a card can be played
+
+        if playable:
+            #voir si on rajoute les priorités
+            self.list_changed[(self.c_turn)%self.nb_joueurs]=[]
+            self.actions[(self.c_turn)%self.nb_joueurs]= "p%d"%playable[0][0]
+            self.c_turn=self.c_turn+1
+            return "p%d"%playable[0][0]
+
+
+        #beginning the strategy
+
+        if game.blue_coins>0:
+
+            interesting=[]
+            #voir ligne 355 deck
+
+            for (i,card) in enumerate(self.other_hands[0].cards):
+                if game.piles[card.color]+1 == card.number:
+                    interesting.append([i+1,card])
+
+
+            if interesting:
+                for p in interesting:
+                    count_rank=0
+                    count_color=0
+                    for card in self.other_hands[0].cards:
+                        if card.number==p[1].number:
+                            count_rank+=1
+                        if card.color==p[1].color:
+                            count_color+=1
+                    if p[1].number_clue is False and count_rank==1:
+                        self.list_changed[(self.c_turn+1)%self.nb_joueurs].append(p)
+                        self.actions[(self.c_turn)%self.nb_joueurs]= "c%d"%p[1].number
+                        self.c_turn=self.c_turn+1
+                        return "c%d"%p[1].number
+                    if p[1].color_clue is False and count_color==1:
+                        clue="c%s"%p[1].color
+                        clue=clue[:2]
+                        self.list_changed[(self.c_turn+1)%self.nb_joueurs].append(p)
+                        self.actions[(self.c_turn)%self.nb_joueurs]= clue
+                        self.c_turn=self.c_turn+1
+                        return clue
+
+
+
+
+
+
+    def always_playable(self,deduction): 
+
+        """
+        give a list of cards that are always playable however the current and the other players are playing/deducting
+
+        """
+        
+        game  = self.game
+        always_playable=[]
+
+        i=0
+        for card in deduction:
+            play=True
+            for rank in card[0]:
+                for color in card[1]:
+                    if game.piles[color]+1!=rank:
+                        play=False
+                        break
+                if play == False: break
+            if play :
+                always_playable.append([i+1,game.current_hand.cards[i]])
+            i+=1
+        return always_playable
+
+    def always_discardable(self,hand): #with game.current_hand
+        """
+        gives a list of cards that are always discardable however the current/other players are playing/deducting
+        """
+
+        game = self.game
+        always_discardable=[]
+
+        #spotting the cards for which we have two clues then filter
+        for (i,card) in enumerate(hand.cards):
+            if card.color_clue and card.number_clue :
+		#checking if discardable
+                if game.piles[card.color]>=card.number :
+                    always_discardable.append([i+1,card])
+                if card in self.other_players_cards :
+                    #voir si on garde dans discardable
+                    always_discardable.append([i+1,card])
+
+
+        #if every rank in the piles is above the rank of one of our card : with one clue only
+
+        liste_rank=list(game.piles.values())
+        for (i,card) in enumerate(hand.cards):
+            discardable=True
+            if card.number_clue: #not necessary but more visible
+                for rank in liste_rank:
+                    if card.number_clue>rank:
+                        discardable=False
+                if discardable:
+                    always_discardable.append([i+1,card])
+
+            if game.piles[card.color_clue]==5:
+                always_discardable.append([i+1,card])
+
+        #if the color itself is discardable because all the cards of a rank above the rank of the pile have already been played/discarded
+
+        colorIds={'Red' : 0,'Blue' : 1,'Green' : 2,'White' : 3,'Yellow' : 4}
+        counter=self.counter()
+        #for (i,card) in enumerate(hand.cards):
+         #   if card.color_clue:
+          #      #picking the list corresponding to the card's color
+           #     count= counter[colorIds[str(card.color_clue)]]
+            #    for remaining in count:
+                 #   if remaining==0 and ####TO BE CONTINUED
+             #           alway_discardable.append([i+1,card])
+        return always_discardable
+     
+
+    #see if we find anything else
+    def counter(self):
+        """
+        look at the table and count the cards played and discared
+        """
+        counter=[[3,2,2,2,1] for i in range(5)]
+        colorIds={'Red' : 0,'Blue' : 1,'Green' : 2,'White' : 3,'Yellow' : 4}
+        game = self.game
+        for card in game.discard_pile.cards:
+            tmp=colorIds[str(card.color)]
+            counter[tmp][card.number-1]=counter[tmp][card.number-1]-1
+            for (color,i) in list(game.piles.items()):
+                if (i!=0) :
+                    for j in range(1,i+1):
+                        tmp=colorIds[str(color)]
+                        counter[tmp][j-1]=counter[tmp][j-1]-1
+        return counter
+
+
+    def global_counter(self):
+        """
+        look at the table and count the cards played and discared by rank only
+        """
+        game=self.game
+        ctr=self.counter()
+        #count is a tab of size 5 which contains the number of remaining card for the ranks 1,2,3,4,5 (in this order)
+        count=[15,10,10,10,5]
+        for i in range(5):
+            for color in ctr:
+                count[i]-=color[i]
+
+
+        return count
+
+
+    
+    def deduction(self):
+        "deduct new clues from the others"
+        game = self.game
+        counter=self.counter()
+        rank=[1,2,3,4,5]
+        Color=hanabi.deck.Color
+        colors=list(Color)
+        deduction=[[rank,colors] for i in game.current_hand.cards]
+        clue_rk=[[],[]]
+        #deductions from clues
+        i=0
+        for card in game.current_hand.cards:
+            if card.color_clue!=False:
+                deduction[i][1]=[card.color]
+                clue_rk[1].append(i)
+            if card.number_clue!=False:
+                deduction[i][0]=[card.number]
+                clue_rk[0].append(i)
+            i+=1
+        #creation of a new counter which takes into account the cards of the others
+        colorIds={'Red' : 0,'Blue' : 1,'Green' : 2,'White' : 3,'Yellow' : 4}
+        new_counter=counter
+        for card in self.other_players_cards:
+            tmp=colorIds[str(card.color)]
+            new_counter[tmp][card.number-1]=new_counter[tmp][card.number-1]-1
+        #deductions from counter
+        current_hand=game.current_hand
+        for i in clue_rk[0]:
+            card=current_hand.cards[i]
+            nb=card.number
+            for j in list(Color):
+                tmp=colorIds[str(j)]
+                if new_counter[tmp][nb-1]==0 and j in deduction[i][1]:
+                    print(j)
+                    print(deduction[i][1])
+                    deduction[i][1].remove(j)
+        for i in clue_rk[1]:
+            card=current_hand.cards[i]
+            color=card.color
+            tmp=colorIds[str(color)]
+            for j in range(1,6):
+                if new_counter[tmp][j-1]==0 and j in deduction[i][0]:
+                    deduction[i][0].remove(j)
+        return deduction
+    
